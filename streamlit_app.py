7b6716ba86e18b8a717c341ded8f356f35315240
tamiyokeyaki <122135530+keyaki456@users.noreply.github.com>
2024-11-02 08:08:02 +0000

    有林

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,8 @@ nyuuryokubayashi=0
 output=""
 tmpcount=0
 
+if input=="":input="外星人かわいい"
+
 aribayashiturple=("0","1","2","3","4","5","6","7","8","9",
                   "a","b","c","d","e","f","g","h","i","j",
                   "k","l","m","n","o","p","q","r","s","t",
